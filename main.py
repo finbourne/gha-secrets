27190c67baca66809ddb3c387e8e9c332c801ce3
Rizwan Saeed <rizwan.saeed@finbourne.com>
2021-12-21 18:03:38 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import os
+import sys
+
+secret = os.environ.get("SECRET_VALUE", None)
+
+if secret == None:
+    print("missing env var SECRET_VALUE")
+    sys.exit(1)
+
+print(f"got secret SECRET_VALUE with value {secret}")
